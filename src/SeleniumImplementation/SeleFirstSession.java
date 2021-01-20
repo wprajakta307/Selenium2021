a9fa39470e3d19cb063729cf54247b5f8c88dfa3
Prajakta Parkar <wprajakta307@gmail.com>
2021-01-20 07:43:30 +0530

    Branch two conflict demo

--- src/SeleniumImplementation/SeleFirstSession.java
+++ src/SeleniumImplementation/SeleFirstSession.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleFirstSession {
 
 	public static void main(String[] args) {
-		
+
+		/* branch two conflict check
+                   it should show conflict after pulling from masters */
 		System.setProperty("webdriver.chrome.driver","G:/study/chromedriver_win32/newDivers/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get(" https://seleniumqtpautomation003.000webhostapp.com/final.html");
